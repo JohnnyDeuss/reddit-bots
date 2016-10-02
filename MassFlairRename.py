9165c904a29c389dfbe56ccaea473cafce0a8387
JohnnyDeuss <johnnydeuss@gmail.com>
2016-10-02 09:06:36 +0200

    Add pagination to MassFlairRename

--- MassFlairRename.py
+++ MassFlairRename.py
@@ -30,25 +30,37 @@ r = praw.Reddit('Python:MassFlairRename by /u/BitwiseShift, run by {}'.format(US
 o = OAuth2Util.OAuth2Util(r)
 o.refresh()
 
-flairs = list(r.get_flair_list(SUBREDDIT, limit=None))
-l = len(flairs)
-unknowns = []
-unknown_count = 0
-# Map flairs to new values.
-for i in range(l-1,-1,-1):
-	# The empty flair texts (None) need to be converted to empty strings.
-	if flairs[i]["flair_text"] == None:
-		flairs[i]["flair_text"] = ""
-	try:
-		flairs[i]["flair_css_class"] = FLAIR_MAPPING[flairs[i]["flair_css_class"]]
-	except KeyError as e:
-		unknown_count += 1
-		# Only give a warning the first time an unknown class is encountered.
-		if flairs[i]["flair_css_class"] not in unknowns:
-			unknowns.append(flairs[i]["flair_css_class"])
-			print('Warning: encountered unknown CSS flair class "{}", ignoring.'.format(flairs[i]["flair_css_class"]))
-		del flairs[i]
-# Only try to update flairs if there are any changes.
-if unknown_count != l:
-	r.set_flair_csv(SUBREDDIT, flairs)
-print('Succesfully changed  {}/{} user flairs!'.format(l-unknown_count, l))
+after = None
+
+print("Getting users...")
+while True:
+	flairs = list(r.get_flair_list(SUBREDDIT, sort="new", limit=None, params={"after": after}))
+	if not flairs:
+		break
+	lastUser = r.get_redditor(flairs[-1]["user"])	# Already set the after, for pagination.
+	after = "t2_"+lastUser.id
+
+	l = len(flairs)
+	unknowns = []
+	unknown_count = 0
+	# Map flairs to new values.
+	for i in range(l-1,-1,-1):
+		# The empty flair texts (None) need to be converted to empty strings.
+		if flairs[i]["flair_text"] == None:
+			flairs[i]["flair_text"] = ""
+		try:
+			flairs[i]["flair_css_class"] = FLAIR_MAPPING[flairs[i]["flair_css_class"]]
+		except KeyError as e:
+			unknown_count += 1
+			# Only give a warning the first time an unknown class is encountered.
+			if flairs[i]["flair_css_class"] not in unknowns:
+				unknowns.append(flairs[i]["flair_css_class"])
+				print('Warning: encountered unknown CSS flair class "{}", ignoring.'.format(flairs[i]["flair_css_class"]))
+			del flairs[i]
+	# Only try to update flairs if there are any changes.
+	if unknown_count != l:
+		r.set_flair_csv(SUBREDDIT, flairs)
+	print("Succesfully changed  {}/{} user flairs!".format(l-unknown_count, l))
+	print("Checking for more flaired users")
+	
+print("No more users found!\nDone!")
